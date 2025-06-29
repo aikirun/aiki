b72c6fe9e6951b0ae5b0b9826d0c44dcbd8f0681
Oluwafemi Shobande <osshobey@gmail.com>
2025-06-29 18:26:54 +0300

    no point dynamically importing deno's delay

--- lib/async/delay.ts
+++ lib/async/delay.ts
@@ -1,23 +1,18 @@
-export async function delay(ms: number, options?: { abortSignal?: AbortSignal }): Promise<void> {
-    if (typeof Deno !== "undefined") {
-      const { delay: denoDelay } = await import("@std/async/delay");
-      return denoDelay(ms, {signal: options?.abortSignal});
-    } else {
-		const abortSignal = options?.abortSignal;
-		if (abortSignal?.aborted) return Promise.reject(abortSignal.reason);
+export function delay(ms: number, options?: { abortSignal?: AbortSignal }): Promise<void> {
+    const abortSignal = options?.abortSignal;
+	if (abortSignal?.aborted) return Promise.reject(abortSignal.reason);
 
-		return new Promise((resolve, reject) => {
-			const abort = () => {
-				clearTimeout(timeout);
-				reject(abortSignal?.reason);
-			};
+	return new Promise((resolve, reject) => {
+		const abort = () => {
+			clearTimeout(timeout);
+			reject(abortSignal?.reason);
+		};
 
-			const timeout = setTimeout(() => {
-				abortSignal?.removeEventListener("abort", abort);
-				resolve();
-			}, ms);
+		const timeout = setTimeout(() => {
+			abortSignal?.removeEventListener("abort", abort);
+			resolve();
+		}, ms);
 
-			abortSignal?.addEventListener("abort", abort, { once: true });
-		});
-    }
+		abortSignal?.addEventListener("abort", abort, { once: true });
+	});
 }
