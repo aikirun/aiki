fd7bfbbe923e0f12d74dd2b0f5afbbebd8c72b4d
Oluwafemi Shobande <osshobey@gmail.com>
2025-06-30 01:57:33 +0300

    and node process type info

--- lib/process/wrapper.node.ts
+++ lib/process/wrapper.node.ts
@@ -1,5 +1,10 @@
-// deno-lint-ignore-file no-process-global
 import type { ProcessWrapper } from "./api.ts";
+
+// deno-lint-ignore-file no-process-global
+declare const process: {
+	on(signal: string, handler: () => void): void;
+	exit(code: number): never;
+};
 
 export const processWrapper: ProcessWrapper = {
 	addSignalListener(signal: string, handler: () => void): void {
