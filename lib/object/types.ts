e7d3c0652a2d3f40373609b319b9440ded8edcea
Oluwafemi Shobande <osshobey@gmail.com>
2025-09-20 17:44:40 +0300

    add RequiredDeep type util

--- lib/object/types.ts
+++ lib/object/types.ts
@@ -21,3 +21,42 @@ type TestUndefinedToPartial = ExpectTrue<
 
 export type MaybeField<Key extends string, Value> = Value extends undefined ? { [K in Key]?: undefined }
 	: { [K in Key]: Value };
+
+export type EmptyObject = Record<PropertyKey, never>;
+
+export type NonArrayObject<T> = T extends object ? (T extends ReadonlyArray<unknown> ? never : T) : never;
+//#region <NonArrayObject Tests>
+type TestNonArrayObjectPlanObject = ExpectTrue<Equal<NonArrayObject<EmptyObject>, EmptyObject>>;
+type TestNonArrayObjectFunction = ExpectTrue<Equal<NonArrayObject<() => unknown>, () => unknown>>;
+type TestNonArrayObjectArray = ExpectTrue<Equal<NonArrayObject<[]>, never>>;
+type TestNonArrayReadonlyArray = ExpectTrue<Equal<NonArrayObject<ReadonlyArray<unknown>>, never>>;
+//#endregion
+
+export type RequiredDeep<T> = NonArrayObject<T> extends never ? T
+	: {
+		[K in keyof T]-?: RequiredDeep<T[K]>;
+	};
+//#region <RequiredDeep Tests>
+type TestRequiredDeep = ExpectTrue<
+	Equal<
+		RequiredDeep<{
+			a?: {
+				b?: string;
+				c: number;
+				d?: {
+					e?: [];
+				};
+			};
+		}>,
+		{
+			a: {
+				b: string;
+				c: number;
+				d: {
+					e: [];
+				}
+			};
+		}
+	>
+>;
+//#endregion
