d090fb055dabb916de1e2fedfcbed459b1f97149
Oluwafemi Shobande <osshobey@gmail.com>
2025-10-10 02:00:25 +0300

    remove unused files

--- server/router.ts
+++
@@ -1,8 +0,0 @@
-import { trpcRouter } from "./context.ts";
-import { workflowRunRouter } from "./controller/workflow-run.ts";
-
-export const appRouter = trpcRouter({
-	workflowRun: workflowRunRouter,
-});
-
-export type AppRouter = typeof appRouter;
