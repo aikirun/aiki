c2155f44fdb0d7a3ace8d69a4966c43681341bae
Oluwafemi Shobande <osshobey@gmail.com>
2025-10-10 01:26:59 +0300

    remote type annotation

--- lib/trigger/strategy.ts
+++ lib/trigger/strategy.ts
@@ -5,7 +5,7 @@ export type TriggerStrategy =
 	| { type: "delayed"; delayMs: number }
 	| { type: "startAt"; startAt: number; };
 
-export const triggerStrategySchema: z.ZodType<TriggerStrategy> = z.discriminatedUnion("type", [
+export const triggerStrategySchema = z.discriminatedUnion("type", [
 	z.object({ type: z.literal("immediate") }),
 	z.object({ type: z.literal("delayed"), delayMs: z.number() }),
 	z.object({ type: z.literal("startAt"), startAt: z.number() }),
