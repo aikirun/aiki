d768ea3c1426af83f5da53c317a8de8a5318aa8a
Oluwafemi Shobande <osshobey@gmail.com>
2025-12-13 05:29:33 +0200

    add todo

--- sdk/client/subscribers/redis-streams.ts
+++ sdk/client/subscribers/redis-streams.ts
@@ -219,7 +219,9 @@ function getRedisStreamConsumerGroupMap(workflows: WorkflowMeta[], shardKeys?: s
 	);
 }
 
-// TODO: attempt to claim stuck messages before fething newer ones
+// TODO: 
+// - attempt to claim stuck messages before fething newer ones
+// - instead of processing streams randomly, try to priority I/O bound streams over CPU hogs
 async function fetchRedisStreamMessages(
 	redis: RedisClient,
 	logger: Logger,
