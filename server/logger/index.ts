3d44b7913733251fcb066ff9c00ff78a78bbfb66
Oluwafemi Shobande <osshobey@gmail.com>
2026-01-09 22:12:14 +0200

    add pino-pretty as runtime dependency

--- server/logger/index.ts
+++ server/logger/index.ts
@@ -1,26 +1,30 @@
 import pino from "pino";
+import pretty from "pino-pretty";
 
 export const logLevels = ["trace", "debug", "info", "warn", "error", "fatal"] as const;
 export type LogLevel = (typeof logLevels)[number];
 
 export function createLogger(logLevel: LogLevel, prettyLogs: boolean) {
-	return pino({
+	const baseOptions: pino.LoggerOptions = {
 		level: logLevel,
 		formatters: {
 			level: (label) => ({ level: label }),
 		},
-		transport: prettyLogs
-			? {
-					target: "pino-pretty",
-					options: {
-						colorize: true,
-						ignore: "pid,hostname",
-						translateTime: "yyyy-mm-dd HH:MM:ss",
-						singleLine: false,
-					},
-				}
-			: undefined,
-	});
+	};
+
+	if (prettyLogs) {
+		return pino(
+			baseOptions,
+			pretty({
+				colorize: true,
+				ignore: "pid,hostname",
+				translateTime: "yyyy-mm-dd HH:MM:ss",
+				singleLine: false,
+			})
+		);
+	}
+
+	return pino(baseOptions);
 }
 
 export type Logger = ReturnType<typeof createLogger>;
