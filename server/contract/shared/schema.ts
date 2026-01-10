decdb0bbf22557bcc4e70b11e5a5679adbb04ed8
Oluwafemi Shobande <osshobey@gmail.com>
2026-01-10 06:15:06 +0200

    Fix, retry strategy, schema,.

--- server/contract/shared/schema.ts
+++ server/contract/shared/schema.ts
@@ -41,11 +41,13 @@ export const retryStrategySchema = type({
 		type: "'exponential'",
 		maxAttempts: "number.integer > 0",
 		baseDelayMs: "number.integer > 0",
-		maxDelayMs: "number.integer > 0",
+		"factor?": "number.integer > 0 | undefined",
+		"maxDelayMs?": "number.integer > 0 | undefined",
 	})
 	.or({
 		type: "'jittered'",
 		maxAttempts: "number.integer > 0",
 		baseDelayMs: "number.integer > 0",
-		maxDelayMs: "number.integer > 0",
+		"jitterFactor?": "number.integer > 0 | undefined",
+		"maxDelayMs?": "number.integer > 0 | undefined",
 	});
