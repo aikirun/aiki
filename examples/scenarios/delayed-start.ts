8e4f03265358744678e3d970d327b9b5fd288065
Oluwafemi Shobande <osshobey@gmail.com>
2026-03-25 22:16:52 +0200

    Remove trigger from workflow definition options. Trigger is a runtime concern, it is the caller's duty specify it.

--- examples/scenarios/delayed-start.ts
+++ examples/scenarios/delayed-start.ts
@@ -1,11 +1,15 @@
-import { runWithWorker } from "../shared/worker";
-import { delayedStartV1 } from "../workflows/delayed-start";
+import { notify } from "examples/workflows/notify";
 
-await runWithWorker([delayedStartV1], async (client) => {
+import { runWithWorker } from "../shared/worker";
+
+await runWithWorker([notify], async (client) => {
 	const startedAt = Date.now();
 	client.logger.info("Starting delayed workflow...");
 
-	const handle = await delayedStartV1.start(client);
+	const handle = await notify
+		.with()
+		.opt("trigger", { type: "delayed", delay: { seconds: 5 } })
+		.start(client, "It's a good day");
 	const result = await handle.waitForStatus("completed");
 
 	if (result.success) {
