75b18574d9395826d1d049b303aca2a1985cd3f9
Oluwafemi Shobande <osshobey@gmail.com>
2026-09-09 01:35:37 +0300

    Add method for clearing in-memory timer priority queue

--- sdk/adapter/memory/src/index.ts
+++ sdk/adapter/memory/src/index.ts
@@ -1,3 +1,4 @@
 export type { InMemoryQueue } from "./queue/in-memory-queue";
 export { inMemoryQueue } from "./queue/in-memory-queue";
+export type { InMemoryTimerPriorityQueue } from "./timer/priority-queue";
 export { inMemoryTimerPriorityQueue } from "./timer/priority-queue";
